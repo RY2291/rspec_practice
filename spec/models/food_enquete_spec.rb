50a2c893f51eb9e72f1291572eb0769996575342
RY2291 <prgr2498@gmail.com>
2021-12-17 13:10:28 +0900

    [fin]バリデーションの検証

---
+++ spec/models/food_enquete_spec.rb
@@ -0,0 +1,190 @@
+require 'rails_helper'
+
+RSpec.describe FoodEnquete, type: :model do
+  describe "正常形の機能" do
+    context "回答する" do
+      it "正しく登録できること 料理：焼きそば food_id:2,
+                          満足度：良い score: 3,
+                          希望するプレゼント:ビール飲み放題 present_id: 1" do
+
+        enquete = FoodEnquete.new(
+          name: '田中 太郎',
+          mail: 'taro.tanaka@example.com',
+          age: 25,
+          food_id: 2,
+          score: 3,
+          request: 'おいしかったです。',
+          present_id: 1
+        )
+
+        expect(enquete).to be_valid
+
+        enquete.save
+
+        answered_enquete = FoodEnquete.find(1);
+
+        expect(answered_enquete.name).to eq("田中 太郎")
+        expect(answered_enquete.mail).to eq('taro.tanaka@example.com')
+        expect(answered_enquete.age).to eq(25)
+        expect(answered_enquete.food_id).to eq(2)
+        expect(answered_enquete.score).to eq(3)
+        expect(answered_enquete.request).to eq('おいしかったです。')
+        expect(answered_enquete.present_id).to eq(1)
+      end
+    end
+  end
+
+
+  describe "入力項目の有無" do
+    context "必須入力項目であること" do
+      it "お名前が必須であること" do
+        new_enquete = FoodEnquete.new
+        expect(new_enquete).not_to be_valid
+        expect(new_enquete.errors[:name]).to include(I18n.t('errors.messages.blank'))
+      end
+
+      it "メールが必須であること" do
+        new_enquete = FoodEnquete.new
+        expect(new_enquete).not_to be_valid
+        expect(new_enquete.errors[:mail]).to include(I18n.t('errors.messages.blank'))
+      end
+
+      it "年齢が必須であること" do
+        new_enquete = FoodEnquete.new
+        expect(new_enquete).not_to be_valid
+        expect(new_enquete.errors[:age]).to include(I18n.t('errors.messages.blank'))
+      end
+
+      it "満足度が必須であること" do
+        new_enquete = FoodEnquete.new
+        expect(new_enquete).not_to be_valid
+        expect(new_enquete.errors[:score]).to include(I18n.t('errors.messages.blank'))
+      end
+
+      it "登録できないこと" do
+        new_enquete = FoodEnquete.new
+        expect(new_enquete.save).to be_falsey
+      end
+    end
+
+    describe 'メールアドレスの形式' do
+      context '不正な形式のメールアドレスの場合' do
+        it 'エラーになること' do
+          new_enquete = FoodEnquete.new
+          new_enquete.mail = "taro.tanaka"
+          expect(new_enquete).not_to be_valid
+          expect(new_enquete.errors[:mail]).to include(I18n.t('errors.messages.invalid'))
+        end
+      end
+    end
+
+    context "任意入力であること" do
+      it "ご意見・ご要望が任意であること" do
+        new_enquete = FoodEnquete.new
+        expect(new_enquete).to be_invalid
+        expect(new_enquete.errors[:request]).not_to include(I18n.t('errors.messages.blank'))
+      end
+    end
+
+  end
+
+  describe "#adult?" do
+    it "20才未満は成人でないこと" do
+      foodEnquete = FoodEnquete.new
+      expect(foodEnquete.send(:adult?, 19)).to be_falsey
+    end
+
+    it "20才以上は成人であること" do
+      foodEnquete = FoodEnquete.new
+      expect(foodEnquete.send(:adult?, 20)).to be_truthy
+    end
+  end
+
+  describe "アンケート回答時の条件" do
+    context "年齢を確認すること" do
+      it "未成年はビール飲み放題を選択できないこと" do
+        enquete_sato = FoodEnquete.new(
+          name: '佐藤 仁美',
+          mail: 'hitomi.sato@example.com',
+          age: 19,
+          food_id: 2,
+          score: 3,
+          request: 'おいしかったです。',
+          present_id: 1   # ビール飲み放題
+        )
+        expect(enquete_sato).not_to be_valid
+        expect(enquete_sato.errors[:present_id]).to include(I18n.t('activerecord.errors.models.food_enquete.attributes.present_id.cannot_present_to_minor'))
+      end
+
+      it '成人はビール飲み放題を選択できないこと' do
+        enquete_sato = FoodEnquete.new(
+          name: '佐藤 仁美',
+          mail: 'hitomi.sato@example.com',
+          age: 20,
+          food_id: 2,
+          score: 3,
+          request: 'おいしかったです。',
+          present_id: 1   # ビール飲み放題
+        )
+        expect(enquete_sato).to be_valid
+      end
+    end
+
+    context "メールアドレスを確認すること" do
+      it "同じメールアドレスで再び回答できないこと" do
+        enquete_tanaka = FoodEnquete.new(
+          name: '田中 太郎',
+          mail: 'taro.tanaka@example.com',
+          age: 25,
+          food_id: 2,
+          score: 3,
+          request: 'おいしかったです。',
+          present_id: 1
+        )
+        enquete_tanaka.save
+
+        re_enquete_tanaka = FoodEnquete.new(
+          name: '田中 太郎',
+          mail: 'taro.tanaka@example.com',
+          age: 25,
+          food_id: 0,
+          score: 1,
+          request: 'おいしかったです。',
+          present_id: 1
+        )
+        expect(re_enquete_tanaka).to be_invalid
+        expect(re_enquete_tanaka.errors[:mail]).to include(I18n.t('errors.messages.taken'))
+        # expect(re_enquete_tanaka).to be_falsey
+        expect(FoodEnquete.all.size).to eq 1
+      end
+
+      it '異なるメールアドレスで回答できること' do
+        enquete_tanaka = FoodEnquete.new(
+          name: '田中 太郎',
+          mail: 'taro.tanaka@example.com',
+          age: 25,
+          food_id: 2,
+          score: 3,
+          request: 'おいしかったです。',
+          present_id: 1
+        )
+        enquete_tanaka.save
+
+        enquete_yamada = FoodEnquete.new(
+          name: '山田 次郎',
+          mail: 'jiro.yamada@example.com',
+          age: 22,
+          food_id: 1,
+          score: 2,
+          request: '',
+          present_id: 0
+        )
+        expect(enquete_yamada).to be_valid
+        enquete_yamada.save
+        expect(FoodEnquete.all.size).to eq 2
+      end
+    end
+  end
+
+
+end
